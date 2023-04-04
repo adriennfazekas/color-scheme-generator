c26f2bea44f3e492248f3823c71a43b922d1ea12
Fazekas Adrienn <adreeja17@gmail.com>
2023-04-04 14:32:20 +0200

    copy the hex code to the clipborad

--- index.js
+++ index.js
@@ -12,14 +12,28 @@ document.getElementById("get-btn").addEventListener("click", function() {
     })
         .then(res=> res.json())
         .then(data=> {
-            console.log(data.colors)
-            data.colors.forEach(color => render(color.hex))            
+            const colors = data.colors
+            console.log(colors)
+            data.colors.forEach(color => renderColor(color.hex)) 
+            data.colors.forEach(color => renderColorName(color.hex))
         })
 })
 
-function render(hex) {
-    const color = hex.value
+function renderColor(hex) {
     feed.innerHTML += `
-        <div class="color-el" style="background-color: ${color};"></div>
+        <div class="color-el" id="${hex.value}" data-copy="${hex.value}" style="background-color: ${hex.value};"></div>
     `
+}
+function renderColorName(hex) {
+    feed.innerHTML += `<div class="color-name" id="${hex.value}" data-copy="${hex.value}">${hex.value}</div>`
+}
+
+document.addEventListener("click", function(e) {
+    if(e.target.dataset.copy) {
+        copyColorName(e.target.dataset.copy)
+    }
+})
+
+function copyColorName(colorId) {
+    navigator.clipboard.writeText(colorId)
 }
